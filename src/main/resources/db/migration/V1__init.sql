7d3dc1ad5328a64e504cd1feabcc76c5545fb645
Mark Glass <mark.glass@gmail.com>
2025-12-22 18:23:51 -0600

    Set up initial project structure with database schema, Flyway migrations, and update to Docker/Jib configurations.

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,5 +1,25 @@
 -- V1__init.sql
-CREATE TABLE example (
-  id BIGSERIAL PRIMARY KEY,
-  name TEXT NOT NULL
+CREATE TABLE recruiter_company
+(
+    id         BIGSERIAL PRIMARY KEY,
+    name       TEXT        NOT NULL,
+    telephone  TEXT        NOT NULL,
+    email      TEXT        NOT NULL,
+    company    TEXT        NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
+
+CREATE INDEX idx_recruiter_company_name ON recruiter_company(name);
+
+CREATE TABLE recruiters
+(
+    id                BIGSERIAL PRIMARY KEY,
+    name              TEXT        NOT NULL,
+    telephone         TEXT        NOT NULL,
+    email             TEXT        NOT NULL,
+    recruiter_company BIGINT REFERENCES recruiter_company (id),
+    created_at        TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at        TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
